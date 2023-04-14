d8d63ffc5de0b5e6a5928565266595a04ffb031b
alefwhite <alefwhite@hotmail.com>
2023-04-14 03:12:29 -0300

    Ok

--- golang-alura/1-Introducao_Go/main.go
+++ golang-alura/1-Introducao_Go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -122,6 +123,7 @@ func menu() {
 			iniciarMonitoramentoArrays()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimiLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -272,7 +274,17 @@ func registraLog(site string, status bool) {
 
 	fmt.Println(arquivo)
 
-	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " " + site + " - Online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - Online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
+}
+
+func imprimiLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
---
+++ golang-alura/2-Poo_Go/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+
+	"poo/contas"
+)
+
+func Somando(numeros ...int) int {
+	resultadoDaSoma := 0
+	for _, numero := range numeros {
+		resultadoDaSoma += numero
+	}
+	return resultadoDaSoma
+}
+
+func main() {
+	var contaDoGuilhermeDois contas.ContaCorrente = contas.ContaCorrente{}
+	contaDoGuilhermeDois.Titular = "Guilherme Dois"
+	fmt.Println(contaDoGuilhermeDois)
+
+	contaDoGuilherme := contas.ContaCorrente{Titular: "Guilherme",
+		NumeroAgencia: 589, NumeroConta: 123456, Saldo: 125.5}
+
+	// Usado quando iremos informar todos os valores para a nossa struct
+	// contaDaBruna := contas.ContaCorrente{"Bruna", 222, 111222, 200}
+
+	contaDaBruna := contas.ContaCorrente{Titular: "Bruna", NumeroAgencia: 222, NumeroConta: 111222, Saldo: 200}
+
+	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDaBruna)
+
+	/*
+		Nil com um tipo definido
+
+		var s *string = nil
+		fmt.Println(s)
+	*/
+
+	// var contaDaCris *contas.ContaCorrente
+	// contaDaCris = new(contas.ContaCorrente)
+
+	contaDaCris := new(contas.ContaCorrente)
+
+	contaDaCris.Titular = "Cris"
+	contaDaCris.Saldo = 500.00
+
+	fmt.Println(contaDaCris)  // Devolve o endereço da variavel
+	fmt.Println(&contaDaCris) // Devolve o local na memoria onde está a variavel
+	fmt.Println(*contaDaCris) // Devolve o conteudo da variavel
+
+	fmt.Println(Somando(1))
+	fmt.Println(Somando(1, 1, 2, 4))
+
+	msg := contaDoGuilherme.Sacar(60.00)
+
+	fmt.Println(msg)
+
+	status, valor := contaDoGuilherme.Depositar(2000)
+
+	fmt.Println(status, valor)
+
+	// Como a conta da cris foi definida como ponteiro na sua declaração e usa o new por padrão a variavel sempre retorna o endereço de onde ela está
+	success := contaDoGuilherme.Transferir(300, contaDaCris)
+
+	// Nesse caso precisamos incluir o & comercial para passar o endereço de onde está a variavel na memoria
+	success2 := contaDoGuilherme.Transferir(300, &contaDaBruna)
+
+	fmt.Println(success, success2)
+	fmt.Println(contaDaCris.Saldo, contaDaBruna.Saldo)
+}
